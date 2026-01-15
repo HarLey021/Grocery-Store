0f8a9ef93aabca1d7e46f47343030b35e1df729b
Davit Kevkhishvili <qevxishvili.dato99@gmail.com>
2026-01-15 23:47:55 +0400

    add necessary folders and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="w-20 h-20 bg-blue-900"></div>
+      <h1 className="text-6xl bg-green-400 text-red-500">Hello</h1>
+    </>
+  );
 }
 
 export default App;
