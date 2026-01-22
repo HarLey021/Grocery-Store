e3902775a14ba40c550c1fc25997360dc545170a
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-23 00:00:45 +0400

    add components for rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Header from "./components/header/Header";
+import Recover from "./components/recover/Recover";
+import Login from "./components/login/Login";
 import Registration from "./components/registration/Registration";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
       <div className="min-h-screen flex-col justify-center items-center">
         <Header />
         <Registration />
+        <Login />
+        <Recover />
       </div>
     </>
   );
