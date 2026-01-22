48fd6004041fceecdbbd870f411d110c0151ccf6
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-22 16:59:25 +0400

    add Registration Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import Registration from "./components/registration/Registration";
 
 function App() {
   return (
     <>
-      <div className="min-h-screen">
+      <div className="min-h-screen flex-col justify-center items-center">
         <Header />
+        <Registration />
       </div>
     </>
   );
