9880e0232eac4e296289daf3332f977a592c9cca
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-28 10:22:05 +0400

    make minor style changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 const Header: React.FC = () => {
   return (
     <>
-      <div className="w-full h-16 bg-white flex justify-between items-center p-4 mb-12">
+      <div className="w-full h-16 bg-white flex justify-between items-center p-4">
         <div className="flex gap-2">
           <img className="w-8 h-8" src="leaf.svg" alt="leaf icon" />
           <h1 className="text-3xl h-8 -mt-0.75">Fresh Market</h1>
--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,24 +1,22 @@
 const Product: React.FC = () => {
   return (
     <>
-      <div className="w-full min-h-screen flex flex-wrap justify-center p-4 gap-6">
-        <div className="w-85 h-81 bg-white rounded-lg p-6">
-          <img
-            className="w-full h-48 rounded-lg mb-4"
-            src="apples.jpg"
-            alt="apples image"
-          />
+      <div className="w-81 h-81 bg-white rounded-lg p-6">
+        <img
+          className="w-full h-48 rounded-lg mb-4"
+          src="apples.jpg"
+          alt="apples image"
+        />
 
-          <div>
-            <h1 className="text-xl text-dark font-normal mb-3">Apples</h1>
+        <div>
+          <h1 className="text-xl text-dark font-normal mb-3">Apples</h1>
 
-            <div className="flex justify-between items-center">
-              <h2 className="text-lg text-green font-normal">GEL4.50/kg</h2>
-              <button className="w-21 h-10 bg-green rounded-lg px-4 py-2 flex justify-between items-center mt-[-4]">
-                <img className="w-4 h-4" src="cart.svg" alt="cart icon" />
-                <h4 className="text-[16px] text-white font-normal">Add</h4>
-              </button>
-            </div>
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg text-green font-normal">GEL4.50/kg</h2>
+            <button className="w-21 h-10 bg-green rounded-lg px-4 py-2 flex justify-between items-center mt-[-4]">
+              <img className="w-4 h-4" src="cart.svg" alt="cart icon" />
+              <h4 className="text-[16px] text-white font-normal">Add</h4>
+            </button>
           </div>
         </div>
       </div>
