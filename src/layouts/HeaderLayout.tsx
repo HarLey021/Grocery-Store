6a7adc88225773287229bfbe3c6683b0a61c24a0
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-01-30 15:51:33 +0400

    change layout styles

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -4,11 +4,15 @@ import Footer from "../components/footer/Footer";
 
 const HeaderLayout: React.FC = () => {
   return (
-    <>
-      <Header />
-      <Outlet />
+    <div className="min-h-screen flex flex-col">
+      <header className="sticky top-0 z-2 bg-white">
+        <Header />
+      </header>
+      <main className="flex-1">
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
