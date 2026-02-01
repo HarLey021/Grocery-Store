31b8340e8b9c35b351697cc9b73283cda0ce30c2
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-01 15:13:48 +0400

    correct typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
-        <div className="w-full h-24 bg-white rounded-lg"></div>
+        <div className="w-full h-24 bg-white rounded-lg [box-shadow:0_4px_20px_rgba(0,0,0,0.1)]"></div>
         <Product />
         <Product />
         <Product />
