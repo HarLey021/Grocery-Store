d8692ecb585bc77b9198aca923a9773690e18911
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-02 20:01:09 +0400

    change component import and make some style changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import Filter from "../components/filter/Filter";
+import SearchBar from "../components/searchBar/SearchBar";
 import Product from "../components/product/Product";
 
 const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
-        <Filter />
+        <SearchBar />
         <Product />
         <Product />
         <Product />
