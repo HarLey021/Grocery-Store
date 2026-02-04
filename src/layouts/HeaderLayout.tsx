454cc447e7d81830aee77468025b6e40bbb760f4
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-04 17:25:38 +0400

    change propagation for correct state management

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,14 +1,31 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import { useContext } from "react";
+import type { MainContextType } from "../types";
+import { MainContext } from "../contexts/MainContext";
 
 const HeaderLayout: React.FC = () => {
+  const { setShowCart, setShowFilter } =
+    useContext<MainContextType>(MainContext);
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="sticky top-0 z-2 bg-white">
+      <header
+        onClick={() => {
+          setShowFilter(false);
+          setShowCart(false);
+        }}
+        className="sticky top-0 z-2 bg-white"
+      >
         <Header />
       </header>
-      <main className="flex-1">
+      <main
+        onClick={() => {
+          setShowFilter(false);
+          setShowCart(false);
+        }}
+        className="flex-1"
+      >
         <Outlet />
       </main>
       <Footer />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,10 @@
 import SearchBar from "../components/searchBar/SearchBar";
 import Product from "../components/product/Product";
-import { useContext } from "react";
-import type { MainContextType } from "../types";
-import { MainContext } from "../contexts/MainContext";
 
 const Home: React.FC = () => {
-  const { setShowFilter } = useContext<MainContextType>(MainContext);
   return (
     <>
-      <div
-        onClick={() => setShowFilter(false)}
-        className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center"
-      >
+      <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
         <SearchBar />
         <Product />
         <Product />
