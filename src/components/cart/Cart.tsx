eef6e3262b9f755028bf060ae5e611725bc0171c
Davit Kevkhishvili <qevxishvili.dato99@gmail.com>
2026-02-09 15:26:35 +0400

    improve design

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -3,25 +3,41 @@ const Cart: React.FC = () => {
     <>
       <div
         onClick={(e) => e.stopPropagation()}
-        className="w-60 min-h-35 bg-white rounded-lg absolute top-17 right-14 lg:top-30 [box-shadow:0_4px_20px_rgba(0,0,0,0.1)] p-3 flex justify-center items-center"
+        className="w-60 min-h-35 bg-white rounded-lg absolute top-17 right-14 lg:top-30 [box-shadow:0_4px_20px_rgba(0,0,0,0.1)] p-3 flex items-center"
       >
         {/* <h3 className="text-[13px] text-light-grey/70 text-center">
           თქვენი კალათა ცარიელია!
         </h3> */}
-        <div className="flex">
+        <div className="w-full h-full flex ">
           <img
-            className="w-10 h-10 rounded-sm"
+            className="w-18 h-18 rounded-lg mr-3"
             src="apples.jpg"
             alt="product image"
           />
-          <div>
-            <h4 className="text-[13px] text-dark font-normal">ვაშლი</h4>
-            <div className="text-[10px] text-grey flex gap-1">
-              <h5>
-                <img className="w-3 h-3" src="lari.svg" alt="" /> 4.50
-              </h5>
-              <span>X</span>
-              <h5>3</h5>
+          <div className="w-full  flex flex-col justify-between">
+            <h4 className="text-[16px] text-dark font-normal">ვაშლი</h4>
+            <div className="text-[12px] text-grey font-normal">
+              <div className="flex gap-1">
+                <div className="flex items-center">
+                  <h5>4.50</h5>
+                  <img
+                    className="w-2.5 h-2.5 -mt-0.5"
+                    src="lari.svg"
+                    alt="lari icon"
+                  />
+                </div>
+                <span className="font-bold">X</span>
+                <h5>4</h5>
+              </div>
+
+              <div className="flex items-center text-[13px] text-dark font-bold">
+                სულ: 18
+                <img
+                  className="w-2.5 h-2.5 -mt-0.5"
+                  src="lari.svg"
+                  alt="lari icon"
+                />
+              </div>
             </div>
           </div>
         </div>
