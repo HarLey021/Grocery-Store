815b09d7afdecbd81dc677428222ca7e77c07b84
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-20 18:16:15 +0400

    change propagation behaviour

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -3,11 +3,11 @@ import CartProduct from "../cartProduct/CartProduct";
 const Cart: React.FC = () => {
   return (
     <>
-      <div className="w-70 absolute top-17 right-14 lg:top-30 lg:right-30 [box-shadow:0_4px_20px_rgba(0,0,0,0.1)] rounded-lg">
-        <div
-          onClick={(e) => e.stopPropagation()}
-          className="w-70 min-h-30 max-h-89.25 bg-white rounded-t-lg   flex flex-col  items-center divide-y divide-[#D1D5DB] overflow-hidden overflow-y-scroll [&::-webkit-scrollbar]:hidden border-b border-[#D1D5DB]"
-        >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="w-70 absolute top-17 right-14 lg:top-30 lg:right-30 [box-shadow:0_4px_20px_rgba(0,0,0,0.1)] rounded-lg"
+      >
+        <div className="w-70 min-h-30 max-h-89.25 bg-white rounded-t-lg   flex flex-col  items-center divide-y divide-[#D1D5DB] overflow-hidden overflow-y-scroll [&::-webkit-scrollbar]:hidden border-b border-[#D1D5DB]">
           {/* <h3 className="text-[13px] text-light-grey/70 text-center">
           თქვენი კალათა ცარიელია!
         </h3> */}
