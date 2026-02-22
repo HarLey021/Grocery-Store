1f29c977776c39182e9256383ef50c54bd5e9a52
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-22 17:37:22 +0400

    add types

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,4 +11,29 @@ type MainContextType = {
   setShowFilter: React.Dispatch<SetStateAction<boolean>>;
   showCart: boolean;
   setShowCart: React.Dispatch<SetStateAction<boolean>>;
+  cartContent: ProductType[];
+  setCartContent: React.Dispatch<SetStateAction<CartItem[]>>;
 };
+
+interface ProductType {
+  [x: string]: ReactNode;
+  id: string;
+  name: string;
+  price: number;
+  unit: "კგ" | "გ" | "ლ" | "მლ" | "ც" | "მგ";
+  defaultAmount: number;
+}
+
+type ProductProps = {
+  key: string;
+  product: ProductType;
+};
+
+type CartProductProps = {
+  key: string;
+  product: ProductType;
+};
+
+interface CartItem extends ProductType {
+  quantity: number;
+}
