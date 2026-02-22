68a13bd75fddbc9305861dbd45903bf28d094c87
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2026-02-22 17:32:06 +0400

    make product data map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,16 @@
 import SearchBar from "../components/searchBar/SearchBar";
 import Product from "../components/product/Product";
+import { products } from "../data";
 
 const Home: React.FC = () => {
   return (
     <>
       <div className="w-full min-h-screen px-6 py-12 lg:px-12 flex flex-wrap gap-12 justify-center">
         <SearchBar />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+
+        {products.map((p) => (
+          <Product key={p.id} product={p} />
+        ))}
       </div>
     </>
   );
